94352f20369a53ab200f5918a6b6b89380aad567
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-08-24 19:54:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// var a = 7; // Declaring a verable
+// let a = 7;
+// document.write(a)
+// console.log(a)
+
+const author = "Ahmed"
+// let author = 5 // throws an error because constant cannot be changed
+let a = 'nawaz';
+{
+    let a = "ahmed"
+    document.write(a)
+}
+
+document.write(" ", a)
+
