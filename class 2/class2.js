093cc4f1f269135ca8da23220450cdedb38255b7
ahmed-nawaz <ahmednawaz3926@gmail.com>
2023-08-24 21:01:57 +0500

    3rd commit

--- class 2/class2.js
+++ class 2/class2.js
@@ -8,24 +8,49 @@
 // Bigint
 // Undefined
 
+// let a = null // wazeh taur par keh rhy hain k iss k andar kuch nhi hai
+// let b = 345
+// let c = true //Boolean // fals bhi ho sakti hai 
+// let d = BigInt("786") + BigInt("3") // Dono integer hi hain 
+// let e = "Ahmed"
+// let f = "symbol" // I'm a nyc symbol
+// let g = undefined // matlab isko koi value assign nhi ki
+// document.write(a, b, c, d, e, f, g)
+// document.write(typeof e)
 
-let a = null // wazeh taur par keh rhy hain k iss k andar kuch nhi hai
-let b = 345
-let c = true //Boolean // fals bhi ho sakti hai 
-let d = BigInt("786") + BigInt("3") // Dono integer hi hain 
-let e = "Ahmed"
-let f = "symbol" // I'm a nyc symbol
-let g = undefined // matlab isko koi value assign nhi ki
-document.write(a, b, c, d, e, f, g)
-document.write(typeof e)
+// // Object // Non primitive deta type  
+// const item = {
+//     "ahmed": true,
+//     "anas": false,
+//     "rafiq": 67,
+//     "farahn": undefined,
+// }
 
-// Object // Non primitive deta type  
+// document.write(item["rafiq"])
 
-const item = {
-    "ahmed": true,
-    "anas": false,
-    "rafiq": 67,
-    "farahn": undefined,
+// Chapter 1 practiec set
+// Q1,
+let a = "ahmed"
+let b = 6
+// document.write(a+b)
+// Q2,
+// document.write(typeof(a+b))
+// Q3,
+const a1 = {
+    name: "harry",
+    section: 54,
+    isPrinciple: false
+}
+// a1 = 45,
+// Q4,
+a1["friend"] = "anas"
+a1["name"] = "khan"
+console.log(a1)
+// Q5,
+const dict = {
+    appreciate: "recognize the full worth of",
+    obstreperous: "noisy, clamorous, or boisterous.",
+    collywobbles: "a feeling of fear, apprehension, or nervousness."
 }
 
-document.write(item["rafiq"])
+console.log(dict["appreciate"])
