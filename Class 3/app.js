d3e1d96e541e89500eade9a74024acd31151725e
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-08-26 19:59:23 +0500

    Class 3

---
+++ Class 3/app.js
@@ -0,0 +1,34 @@
+// Expressions and operators
+// console.log("Operatoprs in JS")
+// let a = 10;
+// let b = 4;
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a / b)
+// console.log(a * b)
+// console.log(a ** b) // The exponentiation ( ** ) operator, a = 45 multiplies into b = 4 (45 multiplies 4 times in this expression)
+// console.log(a % b) // Remandor operator
+// console.log(++a)
+// console.log(a--)
+
+// Assignment operatous
+// a += 8
+// console.log(a)
+
+// Comaprision operatous
+let comp = 6;
+let comp2 = "7";
+console.log(comp == comp2)
+console.log(comp != comp2)
+console.log(comp === comp2)
+console.log(comp !== comp2)
+console.log(comp > comp2)
+
+// Logical operators
+
+let x = 5;
+let y = 6;
+
+console.log(x < y && x == 5)
+console.log(x > y || x == 5)
+console.log(!false)
