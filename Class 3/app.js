62ec239e0075080148203e1097fa99ffca126486
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-08-26 20:32:12 +0500

    Conditionla Expressions

--- Class 3/app.js
+++ Class 3/app.js
@@ -16,19 +16,48 @@
 // console.log(a)
 
 // Comaprision operatous
-let comp = 6;
-let comp2 = "7";
-console.log(comp == comp2)
-console.log(comp != comp2)
-console.log(comp === comp2)
-console.log(comp !== comp2)
-console.log(comp > comp2)
+// let comp = 6;
+// let comp2 = "7";
+// console.log(comp == comp2)
+// console.log(comp != comp2)
+// console.log(comp === comp2)
+// console.log(comp !== comp2)
+// console.log(comp > comp2)
 
 // Logical operators
 
-let x = 5;
-let y = 6;
+// let x = 5;
+// let y = 6;
 
-console.log(x < y && x == 5)
-console.log(x > y || x == 5)
-console.log(!false)
+// console.log(x < y && x == 5)
+// console.log(x > y || x == 5)
+// console.log(!false)
+
+
+// Conditional Expressions
+let a = prompt("What's ypur age")
+console.log(a)
+a = Number.parseInt(a) // Converting the stirng into number
+console.log(typeof(a))
+if(a>0){
+    alert("Thos is a valid age")
+}
+else{
+    alert("Thos is not a valid age");
+}
+
+if (a < 0) {
+    alert("This is a invalid age")
+}
+else if (a < 9) {
+    alert("You are a kid");
+}
+else if (a < 18 && a >= 9) {
+    alert("You are a kid you can think of driving after 18");
+}
+else {
+ alert("You can drive")
+}
+console.log("done")
+
+
