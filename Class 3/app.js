5bf42608a08312d2c3ede11b95f53f4c480a8635
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-08-26 20:32:55 +0500

    Switch Statement

--- Class 3/app.js
+++ Class 3/app.js
@@ -35,29 +35,41 @@
 
 
 // Conditional Expressions
-let a = prompt("What's ypur age")
-console.log(a)
-a = Number.parseInt(a) // Converting the stirng into number
-console.log(typeof(a))
-if(a>0){
-    alert("Thos is a valid age")
-}
-else{
-    alert("Thos is not a valid age");
-}
+// let a = prompt("What's ypur age")
+// console.log(a)
+// a = Number.parseInt(a) // Converting the stirng into number
+// console.log(typeof(a))
+// if(a>0){
+//     alert("Thos is a valid age")
+// }
+// else{
+//     alert("Thos is not a valid age");
+// }
 
-if (a < 0) {
-    alert("This is a invalid age")
-}
-else if (a < 9) {
-    alert("You are a kid");
-}
-else if (a < 18 && a >= 9) {
-    alert("You are a kid you can think of driving after 18");
-}
-else {
- alert("You can drive")
-}
-console.log("done")
+// if (a < 0) {
+//     alert("This is a invalid age")
+// }
+// else if (a < 9) {
+//     alert("You are a kid");
+// }
+// else if (a < 18 && a >= 9) {
+//     alert("You are a kid you can think of driving after 18");
+// }
+// else {
+//  alert("You can drive")
+// }
+// console.log("done")
 
-
+var value = 'Papayas';
+switch (value) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${value}.`);
+}
